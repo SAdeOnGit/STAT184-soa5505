9f4c31cff0ba24e2aacb7d8b48c04d7cba8ba8c0
SchmamOnGIT <Schorboy777@gmail.com>
2024-11-22 19:48:58 -0500

    Create BABYNAMESREADME_directory_metadata..R

---
+++ data/BABYNAMESREADME_directory_metadata..R
@@ -0,0 +1,3 @@
+#This File is meant to display the popularity of Isaac, Hazel and Cynthia as baby names since 1880
+#Cynthia shows up in pink, Isaac is in cyan, and Hazel is shown in green
+#The three names are taken from a much larger baby names data table, with many more names
